a7c4905e9db6e06b9da58e5ccfe6ba4b5655c3b2
Tony Balsamo <tonybalsamo@Tonys-MacBook-Pro-2.local>
2026-03-30 15:12:50 +0800

    fix port for railway

--- app.py
+++ app.py
@@ -84,5 +84,8 @@ def home():
     return render_template_string(HTML, stocks=stocks_data)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
 
